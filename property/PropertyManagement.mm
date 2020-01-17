ea3c675be04c29857f855a519901f7d00bd61706
gwaysoft001 <gwaysoft@outlook.com>
2020-01-17 12:04:54 +0800

    first

---
+++ property/PropertyManagement.mm
@@ -0,0 +1,11 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1579228900774" ID="ID_1827128497" MODIFIED="1579228914934" TEXT="Property Management">
+<node CREATED="1579228916876" ID="ID_1206313557" MODIFIED="1579229023928" POSITION="right" TEXT="repairs">
+<node CREATED="1579229068198" ID="ID_739500371" MODIFIED="1579229083111" TEXT="Jan 17th, 2020">
+<node CREATED="1579229097454" ID="ID_1546705777" MODIFIED="1579229099919" TEXT=" 1. &#x5c0f;&#x533a;&#x53f3;&#x8fb9;&#x55b7;&#x6cc9;&#x58c1;&#x706f;&#x6709;&#x4e00;&#x4e2a;&#x706f;&#x4e0d;&#x4eae;&#xff0c;&#x5468;&#x4e00;&#x5df2;&#x7ecf;&#x63d0;&#x8fc7;&#xff0c;&#x6ca1;&#x6709;&#x7ef4;&#x4fee;&#xff0c;&#x4e5f;&#x672a;&#x8d34;&#x6b63;&#x5728;&#x7ef4;&#x4fee;&#x7684;&#x8b66;&#x793a;"/>
+<node CREATED="1579229112944" ID="ID_430689415" MODIFIED="1579229114910" TEXT="2. 5&#x53f7;&#x697c;B2&#x5927;&#x95e8;&#x524d;&#x5f88;&#x591a;&#x6c34;&#x70b9;&#x6c61;&#x6e0d;&#x6ca1;&#x6709;&#x6e05;&#x7406;&#x5e72;&#x51c0;&#xff0c;&#x901a;&#x8fc7;&#x89c2;&#x5bdf;&#x5df2;&#x7ecf;2&#x4e2a;&#x5468;&#x6ca1;&#x6709;&#x8131;&#x8fc7;&#xff1b;&#x901a;&#x5411;&#x81ea;&#x884c;&#x8f66;&#x5e93;&#x6709;&#x4e00;&#x644a;&#x7591;&#x4f3c;&#x72d7;&#x5c3f;&#xff08;&#x9ec4;&#x7684;&#xff09;&#xff0c;2&#x5929;&#x6ca1;&#x6709;&#x5904;&#x7406;"/>
+</node>
+</node>
+</node>
+</map>
